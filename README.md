074b0a98c24f1d04f12c9530c4fc1791cf708886
chelseawuwu@gmail.com <chelseawuwu@gmail.com>
2023-06-27 22:52:30 +0800

    feat: add support for writing multiple OBJ files from GLB files using pyvista
    This commit adds support for writing multiple OBJ files from GLB files
    using pyvista. The `write-obj.py` script now contains a `WriteObj`
    function that takes in an output OBJ path, a GLB path, and a temporary
    filename. The function imports the GLB file using pyvista, exports it
    as an OBJ file, and saves it to the output OBJ path with the same
    temporary filename.
    
    Additionally, the `write-obj.py` script now contains a `ShowGltf`
    function that takes in a GLB path and a temporary filename. The
    function imports the GLB file using pyvista and displays it using the
    pyvista plotter.
    
    Finally, the `main` function has been added to the `write-obj.py`
    script. This function calls the `WriteObj` function for each temporary
    filename and also calls the `ShowGltf` function for each temporary
    filename.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Write-obj
 Write obj
+
+## Other
+
+### HTML to Embed a 3D Scene??
+
+https://www.rbvi.ucsf.edu/chimerax/data/gltf-viewer-may2023/
+
+
+
+### Coords
+
+
+2.397   6.409  16.381
--- write-obj.py
+++ write-obj.py
@@ -1,8 +1,33 @@
 import pyvista as pv
 
-glb_filename = r"C:\Users\Chelsea\Documents\Mol\mol-data\test.glb"
+def main():
+    out_obj_path = r"C:\Users\Chelsea\Documents\Mol\read-obj\read-obj\Assets\obj\test1"
+    glb_path = r"C:\Users\Chelsea\Documents\Mol\mol-data"
+    tmp_filename = "test"
+    tmp_filename = "test_10"
+    tmp_filenames = ["test", "test_10"]
+    # WriteObj(out_obj_path, glb_path, tmp_filename)
+    for tmp_filename in tmp_filenames:
+        # ShowGltf(glb_path, tmp_filename)
+        WriteObj(out_obj_path, glb_path, tmp_filename)
 
-pl = pv.Plotter()
-pl.import_gltf(glb_filename)
-pl.camera.zoom(1.1)
-pl.show()
+
+def WriteObj(out_obj_path, glb_path, tmp_filename):
+    glb_filename = f"{glb_path}/{tmp_filename}.glb"
+
+    pl = pv.Plotter()
+    pl.import_gltf(glb_filename)
+    pl.camera.zoom(1.1)
+    pl.export_obj(f"{out_obj_path}/{tmp_filename}.obj")
+
+
+def ShowGltf(glb_path, tmp_filename):
+    glb_filename = f"{glb_path}/{tmp_filename}.glb"
+    pl = pv.Plotter()
+    pl.import_gltf(glb_filename)
+    pl.camera.zoom(1.1)
+    print(pl.center)
+    # pl.show()
+
+if __name__ == "__main__":
+    main()
